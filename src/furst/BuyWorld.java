24d6de66d744564ef8998c358c296daf278a938e
Sviatlana Andreyenka <s.andreenko66@gmail.com>
2022-08-23 11:31:22 -0400

    adding the BuyWorld.java

---
+++ src/furst/BuyWorld.java
@@ -0,0 +1,8 @@
+package furst;
+
+public class BuyWorld {
+    public static void main(String[] args) {
+        System.out.println("Maybe 5 weeks of java left");
+        System.out.println("1.5 weeks of git");
+    }
+}
